c23adf802721f4a10249be31bf212d2ad0a0d2ee
alexrribeiro <alexramosribeiro@gmail.com>
2022-02-11 11:58:40 -0300

    Melhorando responsividade e summary

---
+++ teste.js
@@ -0,0 +1,18 @@
+window.addEventListener('resize',abrirEFecharSummary)
+
+function abrirEFecharSummary() {
+
+    console.log('Tá chamando a função');
+    const detail1 = document.getElementById('detail-1');
+    const detail2 = document.getElementById('detail-2');
+    const screen = document.body.clientWidth;
+
+    if (screen <= 768) {
+        detail1.removeAttribute('open');
+        detail2.removeAttribute('open');
+    }
+    else {
+        detail1.setAttribute('open',true);
+        detail2.setAttribute('open',true);
+    }
+}
