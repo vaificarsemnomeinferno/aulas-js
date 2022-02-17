10465226a952252c142ec339202a16fdafe36c8d
alexrribeiro <alexramosribeiro@gmail.com>
2022-02-17 11:37:18 -0300

    Atividades de Exemplo - Parte 5 - Ativ Revisão

--- assets/scripts/gerador.js
+++ assets/scripts/gerador.js
@@ -1,13 +1,56 @@
-const resultado = document.getElementById('resultado');
+const divresultado = document.getElementById('resultado');
+const resultado = document.getElementById('resultado-lista');
+
+const universo = []; // Universo de números (1 a 25 na LF)
+const sorteado = []; // Array com os números sorteados (15 na LF)
+let minimo, maximo, qtdNumeros; // Menor e maior número do universo e quantidade de números sorteados
+
+function criarUniverso(minimo, maximo) {
+
+  for (i = minimo; i <= maximo; i++) {
+    universo[i] = i;
+  }
+  universo.shift();
+
+  return universo;
+}
+
+function sortearNumeros(minimo, maximo, qtdNumeros) {
+  const universo = criarUniverso(minimo, maximo); // Faz um universo
+  maximo = universo.length;
+
+  function compararNumeros(a, b) {
+    return a - b;
+  }
+
+  for (var i = 0; i < qtdNumeros; i++) {
+    posicao = Math.floor(Math.random() * maximo);
+    sorteado[i] = universo[posicao];
+    universo.splice(posicao, 1);
+    maximo = universo.length;
+  }
+
+  sorteado.sort(compararNumeros);
+
+  return sorteado;
+}
+
+function limpar() {
+  universo.splice(0, 100);
+  sorteado.splice(0, 50);
+  minimo = maximo = qtdNumeros = 0;
+  resultado.innerHTML = '';
+}
 
 function gerarNumerosMegaSena() {
-  resultado.style.display = 'flex';
-  resultado.innerHTML = '';
+  limpar();
+  const exibir = sortearNumeros(1, 60, 6);
 
-  resultado.innerHTML += "<div class='teste'><p>1</p></div>";
-  resultado.innerHTML += "<div class='teste'><p>18</p></div>";
-  resultado.innerHTML += "<div class='teste'><p>26</p></div>";
-  resultado.innerHTML += "<div class='teste'><p>37</p></div>";
-  resultado.innerHTML += "<div class='teste'><p>52</p></div>";
-  resultado.innerHTML += "<div class='teste'><p>60</p></div>";
+  // var numerosResultado = document.getElementById("numerosGerados");
+  divresultado.style.display = "flex";
+  // numerosResultado.className = "alert alert-success";
+
+  for (var result of exibir) {
+    resultado.innerHTML += (`<li><span>${result}</span></li>`);
+  }
 }
