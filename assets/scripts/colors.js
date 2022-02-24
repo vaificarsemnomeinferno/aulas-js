ce4987d571a687b63de25acd3aa776906c3840a8
alexrribeiro <alexramosribeiro@gmail.com>
2022-02-24 12:42:00 -0300

    Backup

--- assets/scripts/colors.js
+++ assets/scripts/colors.js
@@ -2,10 +2,15 @@ const navbar = document.querySelector('.navbar');
 const footer = document.querySelector('.footer');
 const titulo = document.querySelector('.titulo');
 const summaryTitle = document.querySelector('.summary-title');
+var cor = document.getElementsByName('colors');
+
+function recarregarPagina() {
+    console.log("oii");
+    location.reload();
+}
 
 function alterarCores() {
     var corEscolhida;
-    var cor = document.getElementsByName('colors');
 
     for (var i = 0; i < cor.length; i++) {
         if (cor[i].checked) {
@@ -13,7 +18,11 @@ function alterarCores() {
             console.log(corEscolhida);
         }
     }
+    preencherComCor(corEscolhida);    
+}
 
+function preencherComCor(corEscolhida) {
+    console.log("entrando aqui", corEscolhida)
     navbar.classList.add(`navbar-${corEscolhida}`);
     footer.classList.add(`footer-${corEscolhida}`);
     titulo.classList.add(`titulo-${corEscolhida}`);
