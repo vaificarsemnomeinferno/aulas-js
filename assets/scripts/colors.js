3bb177093d4d1afc6cb46b03c1a4aa1357ff6bcc
= <=>
2022-02-26 16:37:40 -0300

    Alterar Cores - Funcionando [Quase ok]

--- assets/scripts/colors.js
+++ assets/scripts/colors.js
@@ -3,6 +3,7 @@ const footer = document.querySelector('.footer');
 const titulo = document.querySelector('.titulo');
 const summaryTitle = document.querySelector('.summary-title');
 var cor = document.getElementsByName('colors');
+var corEscolhida, corAnterior;
 
 function recarregarPagina() {
     console.log("oii");
@@ -10,21 +11,23 @@ function recarregarPagina() {
 }
 
 function alterarCores() {
-    var corEscolhida;
 
     for (var i = 0; i < cor.length; i++) {
         if (cor[i].checked) {
             corEscolhida = cor[i].value;
-            console.log(corEscolhida);
         }
     }
     preencherComCor(corEscolhida);    
 }
 
 function preencherComCor(corEscolhida) {
-    console.log("entrando aqui", corEscolhida)
+    navbar.classList.remove(`navbar-${corAnterior}`);
+    footer.classList.remove(`footer-${corAnterior}`);
+    titulo.classList.remove(`titulo-${corAnterior}`);
+    summaryTitle.classList.remove(`summary-title-${corAnterior}`);
     navbar.classList.add(`navbar-${corEscolhida}`);
     footer.classList.add(`footer-${corEscolhida}`);
     titulo.classList.add(`titulo-${corEscolhida}`);
     summaryTitle.classList.add(`summary-title-${corEscolhida}`);
+    corAnterior = corEscolhida;
 }
